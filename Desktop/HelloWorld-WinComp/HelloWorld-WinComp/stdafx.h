28762d4fd1617a0e840f4aecdda435c01a51adf1
Abi Bennet <abbennet@microsoft.com>
2018-12-13 08:29:46 -0800

    Hello World commit

---
+++ Desktop/HelloWorld-WinComp/HelloWorld-WinComp/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#include <Unknwn.h>
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+// reference additional headers your program requires here
+#include <winrt/Windows.UI.Composition.Desktop.h>
+#include <winrt/Windows.System.h>
+#include <DispatcherQueue.h>
+
