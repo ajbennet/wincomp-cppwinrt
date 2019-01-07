99c3bee5dbe8db28bf721b84bee6910935752bdf
Abi Bennet <abbennet@microsoft.com>
2019-01-07 12:46:24 -0800

    D2DInterop first commit

---
+++ UWP/Direct2DInterop/Direct2DInterop/pch.h
@@ -0,0 +1,13 @@
+﻿//
+// pch.h
+// Header for platform projection include files
+//
+
+#pragma once
+#include <Unknwn.h>
+
+#include <windows.h>
+#include "winrt/Windows.ApplicationModel.Core.h"
+#include "winrt/Windows.UI.Core.h"
+#include "winrt/Windows.UI.Composition.h"
+#include "winrt/Windows.UI.Input.h"
